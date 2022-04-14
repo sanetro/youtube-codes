5aba698b63b55e4dc506101fbccda88e2eefaea2
Sanetro <sanetro26@gmail.com>
2022-04-15 01:42:15 +0200

    Secon commit

---
+++ discordBotMovesUsersByRoles.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.members = True
+
+
+bot = commands.Bot(intents=intents, command_prefix='$') 
+TOKEN = "ODgxNjgzODkzNjkzMDY3MzE0.YSwaHQ.taCIUYeCicpRSKGICev_HXug878"
+
+@bot.command()
+async def move(ctx, channel : discord.VoiceChannel, *members : discord.Member):
+    for member in members:
+        await member.move_to(channel)    
+    print("move started")
+
+
+@bot.command()
+async def moveByRole(ctx, channel : discord.VoiceChannel, givenRole : discord.Role):
+    for member in givenRole.members:
+        try:
+            await member.move_to(channel)
+            print(f"User \"{member.name}\" with role \"{givenRole.name}\" moved.")
+        except:
+            print(f"User \"{member.name}\" with role \"{givenRole.name}\" not moved.")    
+   
+@bot.event
+async def on_ready():
+    print("Ready")
+
+bot.run(TOKEN)
+
+
+
+
+
